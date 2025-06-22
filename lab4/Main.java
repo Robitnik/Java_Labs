10f36840457ddc55088fdd984a12c500051d12bc
Robitnik <yuragalat@gmail.com>
2025-06-22 23:45:25 +0300

    Lab4

---
+++ lab4/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num1 = new Scanner(System.in);
+        Scanner num2 = new Scanner(System.in);
+        System.out.println("Enter a number 1: ");
+        int number1 = num1.nextInt();
+        System.out.println("Enter a number 2: ");
+        int number2 = num2.nextInt();
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }    
+}
