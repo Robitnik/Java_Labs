dee760c69863ae1afe2f625773cb8c280cba39e2
Robitnik <yuragalat@gmail.com>
2025-06-22 23:19:48 +0300

    Lab 2

---
+++ lab2/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("hello" + " ");
+        System.out.print("World");
+        System.out.println("!");
+        //Comment
+        /*  
+         * Big comment
+         * 
+         */
+        System.out.println("Hello\nWorld!\t2");
+        System.out.println("Hello\n\"World\"!\t3");
+        System.out.println("Hello\\\n\"World\"!\t4");
+    }   
+}
